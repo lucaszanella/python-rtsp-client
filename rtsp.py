0466d3bfc1546de98adb264deb06b240a9b59299
Lucas Zanella <me@lucaszanella.com>
2017-10-28 17:32:57 -0200

    fixes one line response bug, which was crashing everything

--- rtsp.py
+++ rtsp.py
@@ -180,7 +180,8 @@ class RTSPClient(threading.Thread):
         msg = ''
         if self.cache():
             tmp = self.cache()
-            (msg, tmp) = tmp.split(HEADER_END_STR, 1)
+            print('----' + str(tmp.split(HEADER_END_STR, 1)) + '-----')
+            (msg, tmp) = tmp.split(HEADER_END_STR, 1) if tmp.splitlines()==2 else [tmp, '']
             content_length = self._get_content_length(msg)
             msg += HEADER_END_STR + tmp[:content_length]
             self.set_cache(tmp[content_length:])
